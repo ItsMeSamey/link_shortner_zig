8e2da833aece52c361e6170e84dbc47d27f7d7f0
ItsMeSamey <sameychain5041@gmail.com>
2024-11-21 14:06:18 +0530

    switched to hashmap

--- backend/src/main.zig
+++ backend/src/main.zig
@@ -1,70 +1,196 @@
+test { std.testing.refAllDeclsRecursive(@This()); }
+
 const std = @import("std");
 const linux = std.os.linux;
 
-// The main function to start the server
-pub fn main() void {
-  var addr: linux.sockaddr = @bitCast(linux.sockaddr.in {
-    .family = linux.AF.INET,
-    .port = std.mem.nativeToBig(u16, 8080),
-    .addr = std.mem.nativeToBig(u32, @bitCast([_]u8{ 0, 0, 0, 0 })),
-  });
+// The server struct
+const Server = struct {
+  // fd of Socket that is listening
+  sock: i32,
 
-  const sock: i32 = @intCast(linux.socket(linux.AF.INET, linux.SOCK.STREAM, 0));
-  if (sock == 0) return std.debug.print("socket creation failed\n", .{});
-  defer _ = linux.close(sock);
+  // init the server
+  pub fn init(comptime ip: [4]u8, comptime port: u16) !Server {
+    const addr: linux.sockaddr = @bitCast(linux.sockaddr.in {
+      .family = linux.AF.INET,
+      .addr = std.mem.nativeToBig(u32, @bitCast(ip)),
+      .port = std.mem.nativeToBig(u16, port),
+    });
 
-  var err: usize = undefined;
+    const sock: i32 = @intCast(linux.socket(linux.AF.INET, linux.SOCK.STREAM, 0));
+    if (sock == 0) return std.debug.print("socket creation failed\n", .{});
+    errdefer _ = linux.close(sock);
 
-  err = linux.setsockopt(sock, linux.SOL.SOCKET, linux.SO.REUSEADDR, &std.mem.toBytes(@as(u32, 1)), @sizeOf(u32));
-  if (err != 0) return std.debug.print("setsockopt failed ({x})\n", .{ err });
+    var err: usize = undefined;
 
-  err = linux.bind(sock, &addr, @sizeOf(@TypeOf(addr)));
-  if (err != 0) return std.debug.print("socket bind failed ({x})\n", .{ err });
-
-  err = linux.listen(sock, linux.SOMAXCONN);
-  if (err != 0) return std.debug.print("listen failed ({d})\n", .{ err });
-
-  while (true) {
-    const client: i32 = @intCast(linux.accept(sock, null, null));
-    if (client < 0) {
-      std.debug.print("accept failed with {x}\n", .{ client });
-      continue;
+    // Make the socket reusable
+    err = linux.setsockopt(sock, linux.SOL.SOCKET, linux.SO.REUSEADDR, &std.mem.toBytes(@as(u32, 1)), @sizeOf(u32));
+    if (err != 0) {
+      std.debug.print("setsockopt failed ({x})\n", .{ err });
+      return error.SocketOptionError;
     }
-    defer _ = linux.close(client);
 
-    var buf: [2048]u8 = undefined;
+    err = linux.bind(sock, &addr, @sizeOf(@TypeOf(addr)));
+    if (err != 0) {
+      std.debug.print("socket bind failed ({x})\n", .{ err });
+      return error.SocketBindError;
+    }
+
+    err = linux.listen(sock, linux.SOMAXCONN);
+    if (err != 0) {
+      std.debug.print("listen failed ({d})\n", .{ err });
+      return error.ListenError;
+    }
+
+    return .{ .sock = sock };
+  }
+
+  // Wait for accept and return the client and request
+  pub fn accept(self: *Server, buf: []u8) !struct{ client: i32, request: []const u8 } {
+    const client: i32 = @intCast(linux.accept(self.sock, null, null));
+    if (client < 0) return error.AcceptError;
+    errdefer _ = linux.close(client);
 
     const n = linux.read(client, &buf, buf.len);
-    if (n == 0) {
-      std.debug.print("read failed with {x}\n", .{ n });
-      continue;
-    }
+    if (n == 0) return error.ReadError;
 
-    const response = getResponse(buf[0..n]) catch |e| {
+    return .{ .client = client, .request = buf[0..n] };
+  }
+
+  // Deinit the server
+  pub fn deinit(self: *Server) void {
+    _ = linux.close(self.sock);
+  }
+};
+
+// The value struct that is stored in the values array
+const Value = struct {
+  // The redirect destination
+  dest: []const u8,
+  // Time (in seconds) after which an entry must die
+  deathat: u32,
+};
+
+const Trie = struct {
+  map: Map,
+
+  const Map = std.HashMap([]const u8, Value, struct {
+    fn hash(_: @This(), key: []const u8) u64 {
+      var retval: u64 = 0;
+      for (0.., key) |i, c| {
+        retval = (retval << (i&7)) ^ (retval >> (64 ^ (i&63)));
+        retval ^= c;
+      }
+      return retval;
+    }
+    fn eql(_: @This(), a: []const u8, b: []const u8) bool {
+      if (a.len != b.len) return false;
+
+      if (a.len <= 16) {
+        if (a.len < 4) {
+          const x = (a[0] ^ b[0]) | (a[a.len - 1] ^ b[a.len - 1]) | (a[a.len / 2] ^ b[a.len / 2]);
+          return x == 0;
+        }
+        var x: u32 = 0;
+        for ([_]usize{ 0, a.len - 4, (a.len / 8) * 4, a.len - 4 - ((a.len / 8) * 4) }) |n| {
+          x |= @as(u32, @bitCast(a[n..][0..4].*)) ^ @as(u32, @bitCast(b[n..][0..4].*));
+        }
+        return x == 0;
+      }
+
+      const Scan = if (std.simd.suggestVectorLength(u8)) |vec_size| struct {
+        pub const size = vec_size;
+        pub const Chunk = @Vector(size, u8);
+        pub inline fn isNotEqual(chunk_a: Chunk, chunk_b: Chunk) bool {
+          return @reduce(.Or, chunk_a != chunk_b);
+        }
+      } else struct {
+        pub const size = @sizeOf(usize);
+        pub const Chunk = usize;
+        pub inline fn isNotEqual(chunk_a: Chunk, chunk_b: Chunk) bool {
+          return chunk_a != chunk_b;
+        }
+      };
+      inline for (1..6) |s| {
+        const n = 16 << s;
+        if (n <= Scan.size and a.len <= n) {
+          const V = @Vector(n / 2, u8);
+          var x = @as(V, a[0 .. n / 2].*) ^ @as(V, b[0 .. n / 2].*);
+          x |= @as(V, a[a.len - n / 2 ..][0 .. n / 2].*) ^ @as(V, b[a.len - n / 2 ..][0 .. n / 2].*);
+          const zero: V = @splat(0);
+          return !@reduce(.Or, x != zero);
+        }
+      }
+      for (0..(a.len - 1) / Scan.size) |i| {
+        const a_chunk: Scan.Chunk = @bitCast(a[i * Scan.size ..][0..Scan.size].*);
+        const b_chunk: Scan.Chunk = @bitCast(b[i * Scan.size ..][0..Scan.size].*);
+        if (Scan.isNotEqual(a_chunk, b_chunk)) return false;
+      }
+
+      const last_a_chunk: Scan.Chunk = @bitCast(a[a.len - Scan.size ..][0..Scan.size].*);
+      const last_b_chunk: Scan.Chunk = @bitCast(b[a.len - Scan.size ..][0..Scan.size].*);
+      return !Scan.isNotEqual(last_a_chunk, last_b_chunk);
+    }
+  }, std.hash_map.default_max_load_percentage);
+
+  pub fn init(allocator: std.mem.Allocator) Trie {
+    return .{
+      .map = Map.init(allocator),
+    };
+  }
+
+  pub fn add(self: *Trie, location: []const u8, dest: []const u8, deathat: u32) !void {
+    return self.map.put(location, .{
+      .dest = dest,
+      .deathat = deathat,
+    });
+  }
+
+  pub fn lookup(self: *Trie, location: []const u8) ?[]const u8 {
+    return if (self.map.get(location)) |v| v.dest else null;
+  }
+
+  pub fn remove(self: *Trie, location: []const u8) void {
+    _ = self.map.remove(location);
+  }
+
+  pub fn deinit(self: *Trie) void {
+    self.map.deinit();
+  }
+};
+
+var server: Server = undefined;
+var trie: Trie = undefined;
+
+// The main function to start the server
+pub fn main() void {
+  comptime if (@import("builtin").os.tag != .linux) @compileError("Only Linux is supported");
+
+  server = try Server.init(.{ 0, 0, 0, 0 }, 8080);
+
+  const gpa = std.heap.GeneralPurposeAllocator(.{}){};
+  trie = Trie.init(gpa.allocator());
+
+  while (true) {
+    var buf: [2048]u8 = undefined;
+
+    const client, const request = server.accept(buf[0..]) catch |e| {
+      std.debug.print("accept failed with {x}\n", .{ e });
+      continue;
+    };
+
+    const response = getResponse(request) catch |e| {
       const resp = switch (e) {
         error.Ok => "HTTP/1.1 200\r\n\r\n",
         else => "HTTP/1.1 400\r\n\r\n"
       };
       _ = linux.write(client, resp, resp.len);
-      continue;
+      return error.GetResponseError;
     };
 
     _ = linux.write(client, "HTTP/1.1 302\r\nContent-Length:0\r\nLocation:", 41);
     _ = linux.write(client, response.ptr, response.len);
     _ = linux.write(client, "\r\n\r\n", 4);
   }
-
-  std.time.sleep(1000_000_000_0);
-}
-
-var trie: std.ArrayList(ValueEntry) = undefined;
-var values: std.ArrayList(Value) = undefined;
-
-fn init() void {
-  const allocator = std.testing.allocator;
-
-  trie = std.ArrayList(ValueEntry).init(allocator);
-  values = std.ArrayList(Value).init(allocator);
 }
 
 // Get then redirection for the request
@@ -91,106 +217,4 @@ fn getResponse(input: []const u8) ![]const u8 {
   return "https://ziglang.org";
 }
 
-// Identify weather the entry is a value (one that can have children) or a terminal entry
-const EntryTag = enum(u1) {
-  value,
-  terminal,
-};
-
-// The terminal entry struct
-const TerminalEntry = packed struct {
-  // Offset of the value in the values array
-  valueOffset: u32,
-  // Kinda like short string optimization
-  string: GetSizedArraylikeStruct(u7, 18),
-  // Padding to make size same as ValueEntry
-  padding: u1 = undefined,
-  // Entry type tag
-  tag: EntryTag = .terminal,
-};
-
-// The value entry struct
-const ValueEntry = packed struct {
-  // Offset of the value in the values array
-  valueOffset: u32,
-  // Offset from here (this entry) to the next entry
-  nextOffset: u32,
-  // which children exist
-  fragments: GetSizedArraylikeStruct(u1, UrlTableSize) = GetSizedArraylikeStruct(u1, UrlTableSize).zeroValue(),
-  // Number of bits set in fragments
-  bitcount: u7 = 0,
-  // Entry type tag
-  tag: EntryTag = .value,
-};
-
-// A packed struct cant contain an array, so this is a workaround
-fn GetSizedArraylikeStruct(comptime T: type, comptime size: usize) type {
-  comptime std.debug.assert(@sizeOf(T) != 0);
-  return packed struct {
-    underlying: std.meta.Int(.unsigned, @sizeOf(T) * size),
-
-    pub fn get(self: *const @This(), index: usize) T {
-      std.debug.assert(index < size);
-      return std.mem.readPackedIntNative(T, std.mem.asBytes(&self.underlying), @sizeOf(T) * index);
-    }
-
-    pub fn set(self: *@This(), index: usize, value: T) void {
-      std.debug.assert(index < size);
-      return std.mem.writePackedIntNative(T, std.mem.asBytes(&self.underlying), @sizeOf(T) * index, value);
-    }
-
-    pub fn zeroValue() @This() {
-      return .{ .underlying = 0 };
-    }
-  };
-}
-
-// The value struct that is stored in the values array
-const Value = struct {
-  // The redirect destination
-  dest: []const u8,
-  // Time after which an entry must die (in seconds)
-  deathat: u32,
-};
-
-// Total number of allowed characters in the url table
-pub const UrlTableSize: u8 = 88; // = '[' - '!' + 'z' - 'a' + 5
-
-// Converts a character to an index in the url table
-pub fn charToIndex(in: u8) u8 {
-  return switch (in) {
-    '!'...'[' => in - '!',
-    ']' => '[' - '!' + 1,
-    '_' => '[' - '!' + 2,
-    'a'...'z' => '[' - '!' + 3 + in - 'a',
-    '~' => '[' - '!' + 'z' - 'a' + 4,
-    else => return 255
-  };
-}
-
-// Converts an index in the url table back to character
-pub fn indexToChar(in: u8) u8 {
-  return switch (in) {
-    0...'['-'!' => in + '!',
-    '['-'!'+1 => ']',
-    '['-'!'+2 => '_',
-    '['-'!'+3...'['-'!'+'z'-'a'+3 => in + '[' - '!' + 3 + 'a' - 'z',
-    '['-'!'+'z'-'a'+4 => '~',
-    else => return 255
-  };
-}
-
-test "toIndex, fromIndex" {
-  for (0..127) |char| {
-    const idx = charToIndex(@intCast(char));
-    if (idx == 255) continue;
-
-    try std.testing.expect(idx < UrlTableSize);
-    try std.testing.expect(indexToChar(idx) == char);
-  }
-}
-
-test {
-  std.testing.refAllDeclsRecursive(@This());
-}
 
