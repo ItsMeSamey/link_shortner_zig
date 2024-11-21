37c2b3d30aa20c549370b3f8380b95e343a10e82
ItsMeSamey <sameychain5041@gmail.com>
2024-11-21 11:57:45 +0530

    docs + array workaround

--- backend/src/main.zig
+++ backend/src/main.zig
@@ -1,6 +1,7 @@
 const std = @import("std");
 const linux = std.os.linux;
 
+// The main function to start the server
 pub fn main() void {
   var addr: linux.sockaddr = @bitCast(linux.sockaddr.in {
     .family = linux.AF.INET,
@@ -56,6 +57,17 @@ pub fn main() void {
   std.time.sleep(1000_000_000_0);
 }
 
+var trie: std.ArrayList(ValueEntry) = undefined;
+var values: std.ArrayList(Value) = undefined;
+
+fn init() void {
+  const allocator = std.testing.allocator;
+
+  trie = std.ArrayList(ValueEntry).init(allocator);
+  values = std.ArrayList(Value).init(allocator);
+}
+
+// Get then redirection for the request
 fn getResponse(input: []const u8) ![]const u8 {
   if (input.len < 14) return error.BadRequest;
 
@@ -79,60 +91,102 @@ fn getResponse(input: []const u8) ![]const u8 {
   return "https://ziglang.org";
 }
 
+// Identify weather the entry is a value (one that can have children) or a terminal entry
 const EntryTag = enum(u1) {
   value,
   terminal,
 };
 
+// The terminal entry struct
 const TerminalEntry = packed struct {
+  // Offset of the value in the values array
   valueOffset: u32,
-  string: [18]u7,
-  padding: u1,
-  tag: EntryTag,
+  // Kinda like short string optimization
+  string: GetSizedArraylikeStruct(u7, 18),
+  // Padding to make size same as ValueEntry
+  padding: u1 = undefined,
+  // Entry type tag
+  tag: EntryTag = .terminal,
 };
 
+// The value entry struct
 const ValueEntry = packed struct {
+  // Offset of the value in the values array
   valueOffset: u32,
+  // Offset from here (this entry) to the next entry
   nextOffset: u32,
-  fragments: [UrlTableSize]u1,
-  bitcount: u7,
-  tag: EntryTag,
-
-  pub const UrlTableSize: u8 = 88; // = '[' - '!' + 'z' - 'a' + 5
-
-  pub fn CharToIndex(in: u8) u8 {
-    return switch (in) {
-      '!'...'[' => in - '!',
-      ']' => '[' - '!' + 1,
-      '_' => '[' - '!' + 2,
-      'a'...'z' => '[' - '!' + 3 + in - 'a',
-      '~' => '[' - '!' + 'z' - 'a' + 4,
-      else => return 255
-    };
-  }
-
-  pub fn IndexToChar(in: u8) u8 {
-    return switch (in) {
-      0...'['-'!' => in + '!',
-      '['-'!'+1 => ']',
-      '['-'!'+2 => '_',
-      '['-'!'+3...'['-'!'+'z'-'a'+3 => in + '[' - '!' + 3 + 'a' - 'z',
-      '['-'!'+'z'-'a'+4 => '~',
-      else => return 255
-    };
-  }
+  // which children exist
+  fragments: GetSizedArraylikeStruct(u1, UrlTableSize) = GetSizedArraylikeStruct(u1, UrlTableSize).zeroValue(),
+  // Number of bits set in fragments
+  bitcount: u7 = 0,
+  // Entry type tag
+  tag: EntryTag = .value,
 };
 
-var globalTrie: std.ArrayList(ValueEntry) = undefined;
+// A packed struct cant contain an array, so this is a workaround
+fn GetSizedArraylikeStruct(comptime T: type, comptime size: usize) type {
+  comptime std.debug.assert(@sizeOf(T) != 0);
+  return packed struct {
+    underlying: std.meta.Int(.unsigned, @sizeOf(T) * size),
+
+    pub fn get(self: *const @This(), index: usize) T {
+      std.debug.assert(index < size);
+      return std.mem.readPackedIntNative(T, std.mem.asBytes(&self.underlying), @sizeOf(T) * index);
+    }
+
+    pub fn set(self: *@This(), index: usize, value: T) void {
+      std.debug.assert(index < size);
+      return std.mem.writePackedIntNative(T, std.mem.asBytes(&self.underlying), @sizeOf(T) * index, value);
+    }
+
+    pub fn zeroValue() @This() {
+      return .{ .underlying = 0 };
+    }
+  };
+}
+
+// The value struct that is stored in the values array
+const Value = struct {
+  // The redirect destination
+  dest: []const u8,
+  // Time after which an entry must die (in seconds)
+  deathat: u32,
+};
+
+// Total number of allowed characters in the url table
+pub const UrlTableSize: u8 = 88; // = '[' - '!' + 'z' - 'a' + 5
+
+// Converts a character to an index in the url table
+pub fn charToIndex(in: u8) u8 {
+  return switch (in) {
+    '!'...'[' => in - '!',
+    ']' => '[' - '!' + 1,
+    '_' => '[' - '!' + 2,
+    'a'...'z' => '[' - '!' + 3 + in - 'a',
+    '~' => '[' - '!' + 'z' - 'a' + 4,
+    else => return 255
+  };
+}
+
+// Converts an index in the url table back to character
+pub fn indexToChar(in: u8) u8 {
+  return switch (in) {
+    0...'['-'!' => in + '!',
+    '['-'!'+1 => ']',
+    '['-'!'+2 => '_',
+    '['-'!'+3...'['-'!'+'z'-'a'+3 => in + '[' - '!' + 3 + 'a' - 'z',
+    '['-'!'+'z'-'a'+4 => '~',
+    else => return 255
+  };
+}
 
 test "toIndex, fromIndex" {
-  std.debug.print("{d}\n", .{ ValueEntry.UrlTableSize });
   for (0..127) |char| {
-    const idx = ValueEntry.CharToIndex(@intCast(char));
+    const idx = charToIndex(@intCast(char));
     if (idx == 255) continue;
 
-    std.testing.expect(idx < ValueEntry.UrlTableSize);
-    std.testing.expect(ValueEntry.IndexToChar(idx) == char);
+    try std.testing.expect(idx < UrlTableSize);
+    try std.testing.expect(indexToChar(idx) == char);
   }
 }
 
