f67a66d95a1e00e925d52004f2da36e2460802a7
ItsMeSamey <sameychain5041@gmail.com>
2024-11-30 23:24:23 +0530

    updated some stuff

--- build.zig
+++ build.zig
@@ -1,6 +1,8 @@
 const std = @import("std");
 const builtin = @import("builtin");
+const slash = std.fs.path.sep;
 
+// return true if binName is found in the PATH
 fn existsBin(binName: []const u8) bool {
   const path = std.posix.getenvZ("PATH") orelse return false;
   var pathBuf: [std.posix.system.PATH_MAX]u8 = undefined;
@@ -14,8 +16,8 @@ fn existsBin(binName: []const u8) bool {
 
     @memcpy(pathBuf[from..][0..pathFrgment.len], pathFrgment);
     from += pathFrgment.len;
-    if (pathFrgment[pathFrgment.len - 1] != '/') {
-      pathBuf[from] = '/';
+    if (pathFrgment[pathFrgment.len - 1] != slash) {
+      pathBuf[from] = slash;
       from += 1;
     }
     @memcpy(pathBuf[from..][0..binName.len], binName);
@@ -45,6 +47,7 @@ pub fn build(b: *std.Build) void {
   const installDepsAuto = b.step("install_deps", "Install dependencies, automatiaclly detect if bun or npm is in path (bun is preferred)");
   const buildFrontend = b.step("build_frontend", "Build the frontend using npm and vite");
 
+  // Select the proper package manager
   if (existsBin("bun")) {
     installDepsAuto.dependOn(&installDepsBun.step);
 
@@ -59,10 +62,10 @@ pub fn build(b: *std.Build) void {
     buildFrontend.dependOn(&vite.step);
   } else {
     const failStep = b.addFail("No package manager found in PATH, please install bun or npm");
+
     installDepsAuto.dependOn(&failStep.step);
     buildFrontend.dependOn(&failStep.step);
   }
-
 
   // build backend Step
   const exe = b.addExecutable(.{
