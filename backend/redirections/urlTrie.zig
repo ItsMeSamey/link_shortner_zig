b578dcc6565b381fb4d16bab189238911b2f4ac8
ItsMeSamey <sameychain5041@gmail.com>
2024-12-08 00:04:53 +0530

    updated and fixes urltrie

--- backend/redirections/urlTrie.zig
+++ backend/redirections/urlTrie.zig
@@ -139,235 +139,117 @@ test Value {
 }
 
 
-const Entry = union(enum) {
-  nodes: Nodes,
-  terminal: TerminalEntry,
+const Entry = struct {
+  value: ?*Value,
+  nodes: [TotalCharacterCount]?*Entry,
 
-  pub const Nodes = struct {
-    value: ?*Value,
-    nodes: [TotalCharacterCount]?*Entry,
-
-    pub const NullVal = Nodes {
-      .value = null,
-      .nodes = [1]?*Entry{null} ** TotalCharacterCount,
-    };
-  };
-  pub const TerminalEntry = struct {
-    value: ?*Value,
-    strLen: u16,
-    str: [@sizeOf(Nodes) - @sizeOf(usize) - @sizeOf(u16)]u8,
-
-    pub const NullVal = TerminalEntry {
-      .value = null,
-      .strLen = 0,
-      .str = [1]u8{0} ** (@sizeOf(Nodes) - @sizeOf(usize) - @sizeOf(u16)),
-    };
-
-    pub fn setStr(self: *TerminalEntry, str: []const u8) void {
-      self.strLen = @intCast(str.len);
-      @memcpy(self.str[0..str.len], str);
-    }
+  pub const NullVal = Entry {
+    .value = null,
+    .nodes = [1]?*Entry{null} ** TotalCharacterCount,
   };
 
   fn createNodes(allocator: std.mem.Allocator, value: ?*Value) !*Entry {
     const retval = try allocator.create(Entry);
     retval.* = .{
-      .nodes = .{
-        .value = value,
-        .nodes = Nodes.NullVal.nodes,
-      }
+      .value = value,
+      .nodes = NullVal.nodes,
     };
     return retval;
-  }
-
-  fn createTerminal(allocator: std.mem.Allocator, value: ?*Value, str: []const u8) !*Entry {
-    const retval = try allocator.create(Entry);
-    retval.* = .{
-      .terminal = .{
-        .value = value,
-        .strLen = undefined,
-        .str = undefined,
-      },
-    };
-    retval.terminal.setStr(str);
-    return retval;
-  }
-
-  /// Asserts that the url is valid
-  /// Returns the entry that is nonMatching (and is .nodes) or the entry that points to the value
-  pub fn getNonMatching(noalias self: *Entry, noalias location : *[]const u8) *Entry {
-    std.debug.assert(isUrlValid(location.*));
-    var entry = self;
-
-    for (location.*, 0..) |char, i| {
-      switch (entry.*) {
-        .nodes => {
-          const nodes = &entry.nodes.nodes;
-          if (nodes[indexFromCharacter(char)]) |next| {
-            entry = next;
-            continue;
-          }
-        },
-        .terminal => {},
-      }
-      location.* = location.*[i..];
-      return entry;
-    }
-    location.* = location.*[location.len..]; // 0 length slice
-    return entry;
-  }
-
-  /// Asserts that the url is valid
-  /// Adds nodes and returns the last one for all characters in location.
-  /// NOTE: Returned entry may or may not be terminal entry and may or may not have a value already set
-  pub fn addNodes(self: *Entry, location: []const u8, allocator: std.mem.Allocator) !*Entry {
-    std.debug.assert(isUrlValid(location));
-    var entry = self;
-
-    var i: usize = 0;
-    while (i < location.len) : (i += 1) {
-      switch (entry.*) {
-        .nodes => {
-          const nodes = &entry.nodes.nodes;
-          const idx = indexFromCharacter(location[i]);
-          if (nodes[idx]) |val| {
-            entry = val;
-          } else {
-            nodes[idx] = try createNodes(allocator, null);
-          }
-        },
-        .terminal => {
-          const terminal = &entry.terminal;
-          const diff = std.mem.indexOfDiff(u8, terminal.str[0..terminal.strLen], location[i..]) orelse return entry;
-          var oldNode: TerminalEntry = entry.terminal;
-          entry.nodes.nodes = [1]?*Entry{null} ** TotalCharacterCount;
-          errdefer {
-            entry.freeRecursively(allocator);
-            entry.terminal = oldNode;
-          }
-          const newEntry = try entry.addNodes(location[i..][0..diff], allocator);
-
-          entry = newEntry;
-          if (oldNode.strLen == diff) {
-            entry.nodes.value = oldNode.value;
-            const retval = try createTerminal(allocator, null, location[diff..]);
-            entry.nodes.nodes[indexFromCharacter(location[diff])] = retval;
-            return retval;
-          }
-
-          const idxOld = characterFromIndex(oldNode.str[diff]);
-          oldNode.setStr(oldNode.str[diff..oldNode.strLen]);
-          entry.nodes.nodes[idxOld] = try createTerminal(allocator, oldNode.value, oldNode.str[diff+1..]);
-          if (location.len == diff) return entry;
-
-          const idxNew = characterFromIndex(location[diff]);
-          const retNode = try createTerminal(allocator, null, location[diff+1..]);
-          entry.nodes.nodes[idxNew] = retNode;
-          return retNode;
-        },
-      }
-    }
-
-    return entry;
   }
 
   /// Free all the entries and values that are children of this entry (value of this entry is also free'd)
   /// Does not free the entry itself tho
   pub fn freeRecursively(self: *Entry, allocator: std.mem.Allocator) void {
-    var value: ?*Value = undefined;
-    switch (self.*) {
-      .nodes => {
-        for (self.nodes.nodes) |node| {
-          if (node) |n| {
-            n.freeRecursively(allocator);
-            allocator.destroy(n);
-          }
-        }
-        value = self.nodes.value;
-      },
-      .terminal => {
-        value = self.terminal.value;
-      },
-    }
-    if (value) |val| val.free(allocator);
-  }
-
-  /// Searches for an entry and gives the Entry that is isolated (has only one children all the way to termination)
-  /// Asserts that the location exists and that the url is valid
-  pub fn getIsolatedEntry(self: *Entry, location: []const u8) *Entry {
-    std.debug.assert(isUrlValid(location));
-
-    var retval = self;
-    for (location) |char| {
-      switch (retval.*) {
-        .nodes => {
-          const nodes = &retval.nodes.nodes;
-          if (nodes[indexFromCharacter(char)]) |next| {
-            retval = next;
-          } else return retval;
-        },
-        .terminal => return retval,
+    for (self.nodes) |node| {
+      if (node) |n| {
+        n.freeRecursively(allocator);
+        allocator.destroy(n);
       }
     }
-    return retval;
+    if (self.value) |val| val.free(allocator);
   }
 };
 
 test Entry {
   std.testing.refAllDeclsRecursive(Entry);
-
-  try std.testing.expectEqual(@sizeOf(Entry.Nodes), @sizeOf(Entry.TerminalEntry));
-  try std.testing.expectEqual(@bitSizeOf(Entry.Nodes), @bitSizeOf(Entry.TerminalEntry));
 }
 
 pub const Trie = struct {
-  head: Entry = .{ .nodes = Entry.Nodes.NullVal },
+  head: Entry = Entry.NullVal,
   allocator: std.mem.Allocator,
 
-  pub fn getEntry(self: *Trie, constLocation: []const u8) ?*Value {
-    var location = constLocation;
-    const entry = self.head.getNonMatching(&location);
+  pub fn getEntry(self: *Trie, location: []const u8) ?*Value {
+    if (!isUrlValid(location)) return null;
+    var entry = &self.head;
 
-    const retval = switch (entry.*) {
-      .nodes => entry.nodes.value,
-      .terminal => entry.terminal.value
-    };
-    if (location.len == 0) return retval;
-    if (entry.* != .terminal) return null;
-
-    if (!std.mem.eql(u8, entry.terminal.str[0..entry.terminal.strLen], location)) return null;
-
-    return retval;
+    for (location) |char| {
+      if (entry.nodes[indexFromCharacter(char)]) |next| {
+        entry = next;
+      } else {
+        return null;
+      }
+    }
+    return entry.value;
   }
 
   pub fn add(self: *Trie, location: []const u8, dest: []const u8, deathat: u32) !void {
-    const val = try Value.new(self.allocator, dest, deathat);
-    errdefer val.free(self.allocator);
-    const entry = try self.head.addNodes(location, self.allocator);
-    switch (entry.*) {
-      .nodes => {
-        if (entry.nodes.value) |value| value.free(self.allocator);
-        entry.nodes.value = val;
-      },
-      .terminal => {
-        if (entry.terminal.value) |value| value.free(self.allocator);
-        entry.terminal.value = val;
-      },
+    if (!isUrlValid(location)) return error.InvalidUrl;
+
+    var entry = &self.head;
+    for (location) |char| {
+      const idx = indexFromCharacter(char);
+      if (entry.nodes[idx] == null) {
+        entry.nodes[idx] = try Entry.createNodes(self.allocator, null);
+      }
+      entry = entry.nodes[idx].?;
     }
+
+    if (entry.value) |value| value.free(self.allocator);
+    entry.value = try Value.new(self.allocator, dest, deathat);
   }
 
-  pub fn remove(self: *Trie, location: []const u8) void {
-    _ = self;
-    _ = location;
-    return;
+  pub fn remove(self: *Trie, location: []const u8) !void {
+    if (!isUrlValid(location)) return error.InvalidUrl;
+
+    var toDelete = &self.head;
+    var toDeleteIndex: u8 = InvalidIndex;
+    var current = &self.head;
+    for (location) |char| {
+      const idx = indexFromCharacter(char);
+      for (0..TotalCharacterCount) |i| {
+        if (current.nodes[i]) |_| {
+          if (i == idx) continue;
+          toDelete = current;
+          toDeleteIndex = idx;
+          break;
+        }
+      }
+      current = current.nodes[idx] orelse return error.NotFound;
+    }
+
+    if (current.value) |value| {
+      value.free(self.allocator);
+      current.value = null;
+    }
+
+    var hasChildren = false;
+    for (0..TotalCharacterCount) |i| {
+      if (current.nodes[i] != null) {
+        hasChildren = true;
+        break;
+      }
+    }
+
+    if (!hasChildren) {
+      toDelete.nodes[toDeleteIndex].?.freeRecursively(self.allocator);
+      self.allocator.destroy(toDelete.nodes[toDeleteIndex].?);
+      toDelete.nodes[toDeleteIndex] = null;
+    }
   }
 
   pub fn deinit(self: *Trie) void {
     self.head.freeRecursively(self.allocator);
   }
-
-  const Iterator = struct {
-  };
 };
 
 test Trie {
@@ -382,7 +264,7 @@ test Trie {
   try std.testing.expectEqualStrings("hello", trie.getEntry("hello1").?.getDest());
   try std.testing.expectEqualStrings("world", trie.getEntry("hello2").?.getDest());
   try std.testing.expectEqualStrings("world", trie.getEntry("hello3").?.getDest());
-  trie.remove("hello1");
+  try trie.remove("hello1");
 
   try trie.add("hello1", "world", 1024);
   try trie.add("hello2", "hello", 1024);
@@ -390,7 +272,7 @@ test Trie {
   try std.testing.expectEqualStrings("world", trie.getEntry("hello1").?.getDest());
   try std.testing.expectEqualStrings("hello", trie.getEntry("hello2").?.getDest());
   try std.testing.expectEqualStrings("hello", trie.getEntry("hello3").?.getDest());
-  trie.remove("hello2");
+  try trie.remove("hello2");
 
   try trie.add("hello1", "1", 1024);
   try trie.add("hello2", "2", 1024);
@@ -398,7 +280,7 @@ test Trie {
   try std.testing.expectEqualStrings("1", trie.getEntry("hello1").?.getDest());
   try std.testing.expectEqualStrings("2", trie.getEntry("hello2").?.getDest());
   try std.testing.expectEqualStrings("3", trie.getEntry("hello3").?.getDest());
-  trie.remove("hello3");
+  try trie.remove("hello3");
 
   try trie.add("hello1a", "hello", 1024);
   try trie.add("hello2a", "world", 1024);
